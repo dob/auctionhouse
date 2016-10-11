55cce720be7bc2f81202c01a82d377ae18305f38
Doug Petkanics <petkanics@gmail.com>
2016-10-11 16:53:05 -0400

    Added create auction support to the UI.

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,9 +6,19 @@ function setStatus(message) {
   status.innerHTML = message;
 };
 
-function sendCoin() {
+function setAuctionStatus(message) {
+  var status = document.getElementById("auctionstatus");
+  status.innerHTML = message;
+};
+
+function updateAddress() {
+    var address = document.getElementById("address");
+    address.innerHTML = account;
+}
+
+function createAsset() {
     var sn = SampleName.deployed();
-    var recordId = "8938doug3868686.name22";
+    var recordId = document.getElementById("nameToReserve").value;
 
   setStatus("Initiating transaction... (please wait)");
 
@@ -17,12 +27,49 @@ function sendCoin() {
       setStatus("Transaction complete!");
 
       sn.owner.call(recordId).then(function(res) {
-	  setStatus("Owner is: " + res);
+	  if (res === account) {
+	      setStatus("You are the proud owner of the name: " + recordId);
+	  } else {
+	      setStatus("It looks like the owner of that name is: " + res);
+	  }
       });
   }).catch(function(e) {
     console.log(e);
-    setStatus("Error sending coin; see log.");
+    setStatus("Error registering name. See log.");
   });
+};
+
+function createAuction() {
+    var sn = SampleName.deployed();
+    var ah = AuctionHouse.deployed();
+    var marketer = "0x536d6b87f21d8bbf23dd7f33fc3ca90e85cba0b6";
+
+    setAuctionStatus("Initiating auction, please wait.");
+
+    var recordId = document.getElementById("nameToReserve").value;
+    sn.owner.call(recordId).then(function(res) {
+	if (!(res === account)) {
+	    setAuctionStatus("Looks like you don't own that name");
+	    return;
+	}
+	var startingPrice = web3.toWei(parseFloat(document.getElementById("startingPrice").value), "ether");
+	var reservePrice = web3.toWei(parseFloat(document.getElementById("reservePrice").value), "ether");
+	var deadline = web3.eth.blockNumber + parseInt(document.getElementById("deadline").value);
+
+	ah.createAuction(recordId,
+			 "Auction for this unique name!",
+			 sn.address,
+			 recordId,
+			 deadline,
+			 startingPrice,
+			 reservePrice,
+			 10,
+			 marketer,
+			 {from: account}).then(function(txId) {
+
+			     setAuctionStatus("Auction created in transaction: " + txId)
+			 });
+    })
 };
 
 window.onload = function() {
@@ -37,7 +84,8 @@ window.onload = function() {
       return;
     }
 
-    accounts = accs;
-    account = accounts[1];
+      accounts = accs;
+      account = accounts[0];
+      updateAddress();
   });
 }
