48b6cd59ca8040a3fe55cb01e13c3765d20d0e78
Doug Petkanics <petkanics@gmail.com>
2016-10-27 18:04:15 -0400

    Removing tabs

--- contracts/AuctionHouse.sol
+++ contracts/AuctionHouse.sol
@@ -10,35 +10,35 @@ import "Asset.sol";
 contract AuctionHouse {
 
     struct Bid {
-	address bidder;
-	uint256 amount;
-	uint timestamp;
+        address bidder;
+        uint256 amount;
+        uint timestamp;
     }
 
     enum AuctionStatus {Pending, Active, Inactive}
 
     struct Auction {
-	// Location and ownership information of the item for sale
-	address seller;
-	address contractAddress; // Contract where the item exists
-	string recordId;         // RecordID within the contract as per the Asset interface
+        // Location and ownership information of the item for sale
+        address seller;
+        address contractAddress; // Contract where the item exists
+        string recordId;         // RecordID within the contract as per the Asset interface
 
-	// Auction metadata
-	string title;
-	string description;      // Optionally markdown formatted?
-	uint blockNumberOfDeadline;
-	AuctionStatus status;
+        // Auction metadata
+        string title;
+        string description;      // Optionally markdown formatted?
+        uint blockNumberOfDeadline;
+        AuctionStatus status;
 
-	// Distribution bonus
-	uint distributionCut;    // In percent, ie 10 is a 10% cut to the distribution address
-	address distributionAddress; 
+        // Distribution bonus
+        uint distributionCut;    // In percent, ie 10 is a 10% cut to the distribution address
+        address distributionAddress; 
 
-	// Pricing
-	uint256 startingPrice;   // In wei
-	uint256 reservePrice;
-	uint256 currentBid;
+        // Pricing
+        uint256 startingPrice;   // In wei
+        uint256 reservePrice;
+        uint256 currentBid;
 
-	Bid[] bids;
+        Bid[] bids;
     }
 
     Auction[] public auctions;          // All auctions
@@ -61,101 +61,101 @@ contract AuctionHouse {
     event LogFailure(string message);
 
     modifier onlyOwner {
-	if (owner != msg.sender) throw;
-	_;
+        if (owner != msg.sender) throw;
+        _;
     }
 
     modifier onlySeller(uint auctionId) {
-	if (auctions[auctionId].seller != msg.sender) throw;
-	_;
+        if (auctions[auctionId].seller != msg.sender) throw;
+        _;
     }
 
     modifier onlyLive(uint auctionId) {
-	Auction a = auctions[auctionId];
-	if (a.status != AuctionStatus.Active) {
-	    throw;
-	}
+        Auction a = auctions[auctionId];
+        if (a.status != AuctionStatus.Active) {
+            throw;
+        }
 
-	// Auction should not be over
-	if (block.number >= a.blockNumberOfDeadline) {
-	    throw;
-	}
-	_;
+        // Auction should not be over
+        if (block.number >= a.blockNumberOfDeadline) {
+            throw;
+        }
+        _;
     }
     
     function AuctionHouse() {
-	owner = msg.sender;
+        owner = msg.sender;
     }
 
     // Create an auction, transfer the item to this contract, activate the auction
     function createAuction(
-	string _title,
-	string _description,
-	address _contractAddressOfAsset,
-	string _recordIdOfAsset,
-	uint _deadline,   // in blocknumber
-	uint256 _startingPrice,
-	uint256 _reservePrice,
-	uint _distributionCut,
-	address _distributionCutAddress) returns (uint auctionId) {
+                           string _title,
+                           string _description,
+                           address _contractAddressOfAsset,
+                           string _recordIdOfAsset,
+                           uint _deadline,   // in blocknumber
+                           uint256 _startingPrice,
+                           uint256 _reservePrice,
+                           uint _distributionCut,
+                           address _distributionCutAddress) returns (uint auctionId) {
 
-	    // Check to see if the seller owns the asset at the contract
-	    if (!partyOwnsAsset(msg.sender, _contractAddressOfAsset, _recordIdOfAsset)) {
-		LogFailure("Seller does not own this asset");
-		throw;
-	    }
+        // Check to see if the seller owns the asset at the contract
+        if (!partyOwnsAsset(msg.sender, _contractAddressOfAsset, _recordIdOfAsset)) {
+            LogFailure("Seller does not own this asset");
+            throw;
+        }
 
-	    // Check to see if the auction deadline is in the future
-	    if (block.number >= _deadline) {
-		LogFailure("Block number is not in the future");
-		throw;
-	    }
+        // Check to see if the auction deadline is in the future
+        if (block.number >= _deadline) {
+            LogFailure("Block number is not in the future");
+            throw;
+        }
 
-	    // Price validations
-	    if (_startingPrice < 0 || _reservePrice < 0) {
-		LogFailure("StartingPrice or ReservePrice was below zero");
-		throw;
-	    }
+        // Price validations
+        if (_startingPrice < 0 || _reservePrice < 0) {
+            LogFailure("StartingPrice or ReservePrice was below zero");
+            throw;
+        }
 
-	    // Distribution validations
-	    if (_distributionCut < 0 || _distributionCut > 100) {
-		LogFailure("DistributionCut is invalid");
-		throw;
-	    }
+        // Distribution validations
+        if (_distributionCut < 0 || _distributionCut > 100) {
+            LogFailure("DistributionCut is invalid");
+            throw;
+        }
 
-      if (activeContractRecordConcat[strConcat(addrToString(_contractAddressOfAsset), _recordIdOfAsset)] == true) {
-        LogFailure("Item already on auction");
-        throw;
-      }
+        if (activeContractRecordConcat[strConcat(addrToString(_contractAddressOfAsset), _recordIdOfAsset)] == true) {
+            LogFailure("Item already on auction");
+            throw;
+        }
 
-	    auctionId = auctions.length++;
-	    Auction a = auctions[auctionId];
-	    a.seller = msg.sender;
-	    a.contractAddress = _contractAddressOfAsset;
-	    a.recordId = _recordIdOfAsset;
-	    a.title = _title;
-	    a.description = _description;
-	    a.blockNumberOfDeadline = _deadline;
-	    a.status = AuctionStatus.Pending;
-	    a.distributionCut = _distributionCut;
-	    a.distributionAddress = _distributionCutAddress;
-	    a.startingPrice = _startingPrice;
-	    a.reservePrice = _reservePrice;
-	    a.currentBid = _startingPrice;
+        auctionId = auctions.length++;
+        Auction a = auctions[auctionId];
+        a.seller = msg.sender;
+        a.contractAddress = _contractAddressOfAsset;
+        a.recordId = _recordIdOfAsset;
+        a.title = _title;
+        a.description = _description;
+        a.blockNumberOfDeadline = _deadline;
+        a.status = AuctionStatus.Pending;
+        a.distributionCut = _distributionCut;
+        a.distributionAddress = _distributionCutAddress;
+        a.startingPrice = _startingPrice;
+        a.reservePrice = _reservePrice;
+        a.currentBid = _startingPrice;
 
-            auctionsRunByUser[a.seller].push(auctionId);
-            activeContractRecordConcat[strConcat(addrToString(_contractAddressOfAsset), _recordIdOfAsset)] = true;
+        auctionsRunByUser[a.seller].push(auctionId);
+        activeContractRecordConcat[strConcat(addrToString(_contractAddressOfAsset), _recordIdOfAsset)] = true;
 
-            // activeRecordId[a.contractAddress]
-            // auctionWithRecordId[_recordIdOfAsset] = auctionId;
-	    AuctionCreated(auctionId, a.title, a.startingPrice, a.reservePrice);
+        // activeRecordId[a.contractAddress]
+        // auctionWithRecordId[_recordIdOfAsset] = auctionId;
+        AuctionCreated(auctionId, a.title, a.startingPrice, a.reservePrice);
 
-	    return auctionId;
-	}
+        return auctionId;
+    }
 
     function partyOwnsAsset(address _party, address _contract, string _recordId) returns (bool success) {
-	Asset assetContract = Asset(_contract);
-	return assetContract.owner(_recordId) == _party;
+        Asset assetContract = Asset(_contract);
+        return assetContract.owner(_recordId) == _party;
     }
 
     /**
@@ -175,33 +175,33 @@ contract AuctionHouse {
      * []  -> Auction.status (Not included right now)
      */
     function getAuction(uint idx) returns (address, address, string, string, string, uint, uint, address, uint256, uint256, uint256, uint) {
-	Auction a = auctions[idx];
-	if (a.seller == 0) {
+        Auction a = auctions[idx];
+        if (a.seller == 0) {
             throw;
-	}
+        }
 
-	return (a.seller,
-		a.contractAddress,
-		a.recordId,
-		a.title,
-		a.description,
-		a.blockNumberOfDeadline,
-		a.distributionCut,
-		a.distributionAddress,
-		a.startingPrice,
-		a.reservePrice,
-		a.currentBid,
-		a.bids.length
-               );
+        return (a.seller,
+                a.contractAddress,
+                a.recordId,
+                a.title,
+                a.description,
+                a.blockNumberOfDeadline,
+                a.distributionCut,
+                a.distributionAddress,
+                a.startingPrice,
+                a.reservePrice,
+                a.currentBid,
+                a.bids.length
+                );
     }
 
     function getAuctionCount() returns (uint) {
-	return auctions.length;
+        return auctions.length;
     }
 
     function getStatus(uint idx) returns (uint) {
-	Auction a = auctions[idx];
-	return uint(a.status);
+        Auction a = auctions[idx];
+        return uint(a.status);
     }
 
     function getAuctionsCountForUser(address addr) returns (uint) {
@@ -213,7 +213,7 @@ contract AuctionHouse {
     }
 
     function getActiveContractRecordConcat(string _contractRecordConcat) returns (bool) {
-      return activeContractRecordConcat[_contractRecordConcat];
+        return activeContractRecordConcat[_contractRecordConcat];
     }
 
     // Checks if this contract address is the owner of the item for the auction
@@ -223,7 +223,7 @@ contract AuctionHouse {
         if (!partyOwnsAsset(this, a.contractAddress, a.recordId)) throw;
 
         a.status = AuctionStatus.Active;
-	AuctionActivated(auctionId);
+        AuctionActivated(auctionId);
         return true;
     }
 
@@ -231,160 +231,159 @@ contract AuctionHouse {
         Auction a = auctions[auctionId];
 
         if (!partyOwnsAsset(this, a.contractAddress, a.recordId)) throw;
-	if (a.currentBid >= a.reservePrice) throw;   // Can't cancel the auction if someone has already outbid the reserve.
+        if (a.currentBid >= a.reservePrice) throw;   // Can't cancel the auction if someone has already outbid the reserve.
 
         Asset asset = Asset(a.contractAddress);
         if(!asset.setOwner(a.recordId, a.seller)) {
-	    throw;
-	}
+            throw;
+        }
 
-	// Return funds to the bidder
-	uint bidsLength = a.bids.length;
-	if (bidsLength > 0) {
-	    Bid topBid = a.bids[bidsLength - 1];
-	    if (!topBid.bidder.send(topBid.amount)) {
-		LogFailure("Couldn't return funds to the bidder");
-		return false;
-	    }
+        // Return funds to the bidder
+        uint bidsLength = a.bids.length;
+        if (bidsLength > 0) {
+            Bid topBid = a.bids[bidsLength - 1];
+            if (!topBid.bidder.send(topBid.amount)) {
+                LogFailure("Couldn't return funds to the bidder");
+                return false;
+            }
 
-  activeContractRecordConcat[strConcat(addrToString(a.contractAddress), a.recordId)] = false;
-	}
+            activeContractRecordConcat[strConcat(addrToString(a.contractAddress), a.recordId)] = false;
+        }
 
-	AuctionCancelled(auctionId);
+        AuctionCancelled(auctionId);
         a.status = AuctionStatus.Inactive;
-	return true;
+        return true;
     }
 
     /* BIDS */
     function getBidCountForAuction(uint auctionId) returns (uint) {
-	Auction a = auctions[auctionId];
-	return a.bids.length;
+        Auction a = auctions[auctionId];
+        return a.bids.length;
     }
 
     function getBidForAuctionByIdx(uint auctionId, uint idx) returns (address bidder, uint256 amount, uint timestamp) {
-	Auction a = auctions[auctionId];
-	if(idx > a.bids.length - 1) {
-	    throw;
-	}
-	
-	Bid b = a.bids[idx];
-	return (b.bidder, b.amount, b.timestamp);
+        Auction a = auctions[auctionId];
+        if(idx > a.bids.length - 1) {
+            throw;
+        }
+        
+        Bid b = a.bids[idx];
+        return (b.bidder, b.amount, b.timestamp);
     }
 
     function placeBid(uint auctionId) payable onlyLive(auctionId) returns (bool success) {
-	uint256 amount = msg.value;
-	Auction a = auctions[auctionId];
+        uint256 amount = msg.value;
+        Auction a = auctions[auctionId];
 
-	if (a.currentBid >= amount) {
-	    // Want to return the bid amount and return false
-	    if(!msg.sender.send(amount)) {
-		LogFailure("Could not return the invalid bid amount to the bidder");
-	    }
-	    return false;
-	}
+        if (a.currentBid >= amount) {
+            // Want to return the bid amount and return false
+            if(!msg.sender.send(amount)) {
+                LogFailure("Could not return the invalid bid amount to the bidder");
+            }
+            return false;
+        }
 
-	uint bidIdx = a.bids.length++;
-	Bid b = a.bids[bidIdx];
-	b.bidder = msg.sender;
-	b.amount = amount;
-	b.timestamp = now;
-	a.currentBid = amount;
+        uint bidIdx = a.bids.length++;
+        Bid b = a.bids[bidIdx];
+        b.bidder = msg.sender;
+        b.amount = amount;
+        b.timestamp = now;
+        a.currentBid = amount;
 
-	auctionsBidOnByUser[b.bidder].push(auctionId);
+        auctionsBidOnByUser[b.bidder].push(auctionId);
 
-	// Return ETH to previous bidder
-	if (bidIdx > 0) {
-	    Bid previousBid = a.bids[bidIdx - 1];
-	    if (!previousBid.bidder.send(previousBid.amount)) {
-		LogFailure("Could not return the outbid amount to the previous bidder");
-		throw;
-	    }
-	}
+        // Return ETH to previous bidder
+        if (bidIdx > 0) {
+            Bid previousBid = a.bids[bidIdx - 1];
+            if (!previousBid.bidder.send(previousBid.amount)) {
+                LogFailure("Could not return the outbid amount to the previous bidder");
+                throw;
+            }
+        }
 
-	BidPlaced(auctionId, b.bidder, b.amount);
-	return true;
+        BidPlaced(auctionId, b.bidder, b.amount);
+        return true;
     }
 
     function endAuction(uint auctionId) returns (bool success) {
-	// Check if the auction is passed the end date
-	Auction a = auctions[auctionId];
-  activeContractRecordConcat[strConcat(addrToString(a.contractAddress), a.recordId)] = false;
-	
-	if (block.number < a.blockNumberOfDeadline) {
-	    LogFailure("Can not end an auction that hasn't hit the deadline yet");
-	    return false;
-	}
+        // Check if the auction is passed the end date
+        Auction a = auctions[auctionId];
+        activeContractRecordConcat[strConcat(addrToString(a.contractAddress), a.recordId)] = false;
+        
+        if (block.number < a.blockNumberOfDeadline) {
+            LogFailure("Can not end an auction that hasn't hit the deadline yet");
+            return false;
+        }
 
-	Asset asset = Asset(a.contractAddress);
+        Asset asset = Asset(a.contractAddress);
 
-	// No bids, make the auction inactive
-	if (a.bids.length == 0) {
-	    if(!asset.setOwner(a.recordId, a.seller)) {
-		throw;
-	    }
-	    a.status = AuctionStatus.Inactive;
-	    return true;
-	}
+        // No bids, make the auction inactive
+        if (a.bids.length == 0) {
+            if(!asset.setOwner(a.recordId, a.seller)) {
+                throw;
+            }
+            a.status = AuctionStatus.Inactive;
+            return true;
+        }
 
-	Bid topBid = a.bids[a.bids.length - 1];
+        Bid topBid = a.bids[a.bids.length - 1];
 
-	// If the auction hit its reserve price
-	if (a.currentBid >= a.reservePrice) {
-	    uint distributionShare = a.currentBid * a.distributionCut / 100;  // Calculate the distribution cut
-	    uint sellerShare = a.currentBid - distributionShare;
+        // If the auction hit its reserve price
+        if (a.currentBid >= a.reservePrice) {
+            uint distributionShare = a.currentBid * a.distributionCut / 100;  // Calculate the distribution cut
+            uint sellerShare = a.currentBid - distributionShare;
 
-	    if(!asset.setOwner(a.recordId, topBid.bidder)) {
-		throw;
-	    } // Set the items new owner
-	    
-	    if (!a.distributionAddress.send(distributionShare)) {
-		LogFailure("Couldn't send the marketing distribution");
-		throw;
-	    }
+            if(!asset.setOwner(a.recordId, topBid.bidder)) {
+                throw;
+            } // Set the items new owner
+            
+            if (!a.distributionAddress.send(distributionShare)) {
+                LogFailure("Couldn't send the marketing distribution");
+                throw;
+            }
 
-	    if (!a.seller.send(sellerShare)) {
-		LogFailure("Couldn't send the seller his cut");
-		throw;
-	    }
+            if (!a.seller.send(sellerShare)) {
+                LogFailure("Couldn't send the seller his cut");
+                throw;
+            }
 
-	    AuctionEndedWithWinner(auctionId, topBid.bidder, a.currentBid);
-	} else {
-	    // Return the item to the owner and the money to the top bidder
-	    if(!asset.setOwner(a.recordId, a.seller)) {
-		throw;
-	    }
-	    if (!topBid.bidder.send(a.currentBid)) {
-		LogFailure("Couldn't send the top bidder his money back on a failed to meet reserve scenario.");
-		throw;
-	    }
+            AuctionEndedWithWinner(auctionId, topBid.bidder, a.currentBid);
+        } else {
+            // Return the item to the owner and the money to the top bidder
+            if(!asset.setOwner(a.recordId, a.seller)) {
+                throw;
+            }
+            if (!topBid.bidder.send(a.currentBid)) {
+                LogFailure("Couldn't send the top bidder his money back on a failed to meet reserve scenario.");
+                throw;
+            }
 
-	    AuctionEndedWithoutWinner(auctionId, a.currentBid, a.reservePrice);
-	}
+            AuctionEndedWithoutWinner(auctionId, a.currentBid, a.reservePrice);
+        }
 
-	a.status = AuctionStatus.Inactive;
-	return true;
+        a.status = AuctionStatus.Inactive;
+        return true;
     }
 
     function() {
-	// Don't allow ether to be sent blindly to this contract
-	throw;
+        // Don't allow ether to be sent blindly to this contract
+        throw;
     }
 
-  function strConcat(string _a, string _b) internal returns (string) {
-    bytes memory _ba = bytes(_a);
-    bytes memory _bb = bytes(_b);
-    bytes memory ab = new bytes (_ba.length + _bb.length);
-    uint k = 0;
-    for (uint i = 0; i < _ba.length; i++) ab[k++] = _ba[i];
-    for (i = 0; i < _bb.length; i++) ab[k++] = _bb[i];
-    return string(ab);
-  }
+    function strConcat(string _a, string _b) internal returns (string) {
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        bytes memory ab = new bytes (_ba.length + _bb.length);
+        uint k = 0;
+        for (uint i = 0; i < _ba.length; i++) ab[k++] = _ba[i];
+        for (i = 0; i < _bb.length; i++) ab[k++] = _bb[i];
+        return string(ab);
+    }
 
-  function addrToString(address x) returns (string) {
-    bytes memory b = new bytes(20);
-    for (uint i = 0; i < 20; i++)
-        b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
-    return string(b);
-}
-
+    function addrToString(address x) returns (string) {
+        bytes memory b = new bytes(20);
+        for (uint i = 0; i < 20; i++)
+            b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
+        return string(b);
+    }
 }
