e69c3847db5224201c21064d1b2343cbca0ace01
Eric Tang <eric.x.tang@gmail.com>
2016-10-27 15:53:28 -0400

    convert tabs to spaces

--- contracts/SampleName.sol
+++ contracts/SampleName.sol
@@ -7,49 +7,49 @@ import "Asset.sol";
 
 contract SampleName is Asset {
     struct Record {
-	address owner;
-	string name;
-	address walletAddress;
+        address owner;
+        string name;
+        address walletAddress;
     }
 
     mapping(string => Record) records;  // Map an ID to the record
 
     modifier onlyOwner(string _recordId) {
-	if (records[_recordId].owner != msg.sender) throw;
-	_;
+        if (records[_recordId].owner != msg.sender) throw;
+        _;
     }
 
     function owner(string _recordId) returns (address ownerAddress) {
-	return records[_recordId].owner;
+        return records[_recordId].owner;
     }
 
     function setOwner(string _recordId, address _newOwner) onlyOwner(_recordId) returns (bool success) {
-	records[_recordId].owner = _newOwner;
-	return true;
+        records[_recordId].owner = _newOwner;
+        return true;
     }
 
     function addRecord(string _recordId, address _owner, string _name, address _walletAddress) returns (bool sufficient) {
-	if (records[_recordId].owner != 0) {
-	    // If a record with this name already exists
-	    return false;
-	}
+        if (records[_recordId].owner != 0) {
+            // If a record with this name already exists
+            return false;
+        }
 
-	Record r = records[_recordId];
+        Record r = records[_recordId];
 
-	r.owner = _owner;
-	r.name = _name;
-	r.walletAddress = _walletAddress;
-	return true;
+        r.owner = _owner;
+        r.name = _name;
+        r.walletAddress = _walletAddress;
+        return true;
     }
 
     // Allow the owner to update the wallet address of the record
     function updateRecordWalletAddress(string _recordId, address _newWalletAddress) onlyOwner(_recordId) returns (bool success) {
-	if (records[_recordId].owner == 0) {
-	    // We don't know this record
-	    return false;
-	}
+        if (records[_recordId].owner == 0) {
+            // We don't know this record
+            return false;
+        }
 
-	records[_recordId].walletAddress = _newWalletAddress;
-	return true;	
+        records[_recordId].walletAddress = _newWalletAddress;
+        return true;	
     }
 }
