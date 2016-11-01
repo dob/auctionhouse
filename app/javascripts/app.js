4e98343d99040c426e31f9c0051395091548aa2a
Eric Tang <eric.x.tang@gmail.com>
2016-11-01 17:23:03 -0400

    fix auctions loading bug by using js closure

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -2,45 +2,58 @@ var accounts;
 var account;
 var auctions;
 var auctionHouseContract;
-// var currentBlockNumber;
+var aucs = [];
+
+function getAuction(auctionId) {
+    auctionHouseContract.getAuction.call(auctionId).then(function(auction) {
+        console.log("loading: " + auctionId);
+        auction[12] = auctionId;
+        aucs.push(auction);
+    });
+}
+
+function waitAndRefresh(count) {
+    if (aucs.length < count) {
+        console.log("sleeping");
+        setTimeout(waitAndRefresh, 500, count);
+    } else {
+        var auctionSection = document.getElementById("userAuctions");
+        var res = "";
+        for (var j = 0; j < count; j++) {
+            var auc = aucs[j];
+            if (parseInt(auc[5]) > currentBlockNumber) {
+                res = res + "<tr>";
+                res = res + "<td><a href='auction.html?auctionId=" + auc[12] + "'>" + auc[3] + "</a></td>";
+                res = res + "<td>" + web3.fromWei(auc[10], "ether") + " ETH" + "</td>";
+                res = res + "<td>" + auc[11] + "</td>";
+                res = res + "<td>" + auc[5] + "</td>";
+                res = res + "</tr>";
+            }
+        }
+        console.log("Refreshing auctions!");
+        auctionSection.innerHTML = res;
+        setStatus("");
+    }
+}
 
 function updateAuctions() {
-    var auctionSection = document.getElementById("userAuctions");
-    var res = "";
 
     setStatus("Auctions being fetched...", "warning");
 
     auctionHouseContract.getAuctionCount.call().then(function(count) {
-	console.log("Contract has this many auctions " + count);
+    	console.log("Contract has this many auctions " + count);
 
-	if (count <= 0) {
-	    setStatus("No auctions found", "error");
-	}
+    	if (count <= 0) {
+    	    setStatus("No auctions found", "error");
+    	}
 
-	var aucs = [];
-	
-	for (var i = 0; i < count; i++) {
-	    auctionHouseContract.getAuction.call(i).then(function(auction) {
-            // Wrapping in a function because I need to access i
-            aucs.push(auction);
+    	var aucs = [];
+    	
+    	for (var i = 0; i < count; i++) {
+            getAuction(i);
+    	}
 
-            if (aucs.length == count) {
-                for (var j = 0; j < count; j++) {
-                    var auc = aucs[j];
-                    if (parseInt(auc[5]) > currentBlockNumber) {
-                        res = res + "<tr>";
-                        res = res + "<td><a href='auction.html?auctionId=" + j + "'>" + auc[3] + "</a></td>";
-                        res = res + "<td>" + web3.fromWei(auc[10], "ether") + " ETH" + "</td>";
-                        res = res + "<td>" + auc[11] + "</td>";
-                        res = res + "<td>" + auc[5] + "</td>";
-                        res = res + "</tr>";
-                    }
-                }
-                auctionSection.innerHTML = res;
-                setStatus("");
-            }
-	    });
-	}
+        waitAndRefresh(count);
     });    
 }
 
