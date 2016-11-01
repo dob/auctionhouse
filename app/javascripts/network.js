803f653e0691bfea1ec942300857326a5dda3a57
Eric Tang <eric.x.tang@gmail.com>
2016-11-01 12:32:21 -0400

    testnet deployment

--- app/javascripts/network.js
+++ app/javascripts/network.js
@@ -5,7 +5,20 @@ getContractAddress = function(callback) {
             ah_contract_addr = '';
             sn_contract_addr = '';
             error = "Failed to load ethereum network and smart contract";
+        } else if (result == "1" || result == "2") {
+            if (result == "1") {
+                ah_contract_addr = '';
+                sn_contract_addr = '';
+                error("AuctionHouse is not deployed to the main net yet, please try the test net");
+            }
+
+            //Testnet Setup
+            if (result == "2") {
+                ah_contract_addr = "0x131bec75342a54ffea3087bda5ba720394c486a9";
+                sn_contract_addr = "0xfe4362ad1c80bbe89705f774af1d769a0f305605";
+            }
         } else {
+            //For dev purposes - we use Fieldbook as a registry for our local contract address
             var xmlHttp = new XMLHttpRequest();
             xmlHttp.open( "GET", "https://api.fieldbook.com/v1/5802f2556147990300c7827b/sheet_1?network=" + result + "&key=auctionhouse", false );
             xmlHttp.send( null );
